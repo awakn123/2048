857c1900b6d2bd7c94836d67b30646fa6116e39c
Jitong <jtzou0430@gmail.com>
2023-11-19 14:27:56 -0500

    create the class BFS_Agent, Represents an agent that uses Breadth-First Search
    finish the part of constructor
    
    not yet finished: getAction method

---
+++ js/ai_bfsagent.js
@@ -0,0 +1,11 @@
+// Class BFSAgent: Represents an agent that uses Breadth-First Search (BFS)
+class BFSAgent extends AbstractAgent{
+  constructor(props) {
+    super(props);
+    this.name = "BFS"; // Setting the agent name as 'BFS'
+  }
+
+  // getAction: Determines the next action for the agent
+  getAction() {
+    return null;
+  }
