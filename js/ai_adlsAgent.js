109eb426eb2757289d571fb3f4c638cd5e78a85d
caoyun <awakn123@gmail.com>
2023-12-02 22:13:54 -0500

    Implement ADLS

--- js/ai_adlsAgent.js
+++ js/ai_adlsAgent.js
@@ -1,15 +1,27 @@
 class ADLSAgent extends AbstractAgent {
   constructor(props) {
     super(props);
-    this.name = "ADLS"; // Setting the agent name as 'DLS'
+    this.name = 'ADLS';
+    this.averageNum = 5;
+    this.depthLimit = 5;
   }
+
   getAction() {
-    let sim = new SimulationGM(window.gameManager);
-    if (sim.isGameTerminated()) // Check if the game is over
-      return null;
-    // let bestAction = this.dls(0, sim, 3); // Perform DLS to find the best action
-    // return bestAction;
-    return 0;
+    let bestAction = null, bestCnt = 0;
+    for (let i = 0; i < this.averageNum; i++) {
+      let dls = new DLSAgent();
+      dls.depthLimit = this.depthLimit;
+      let action = dls.getAction();
+      if (bestCnt === 0) {
+        bestAction = action;
+        bestCnt++;
+      } else if (action === bestAction) {
+        bestCnt++;
+      } else {
+        bestCnt--;
+      }
+    }
+    return bestAction;
   }
 
 }
